c9227b64af5aa44585d15d10158f62b6a001e9a2
mateus <mateusbona28@gmail.com>
2023-03-22 18:16:28 -0300

    feat: base do projeto concluída, pronto para desenvolvimento de componentes e estados

---
+++ src/styled/GlobalStyle.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  :root{
+    --white0 : #fff;
+    --grey0 : #000
+  }
+
+  * {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+  }
+`
+
